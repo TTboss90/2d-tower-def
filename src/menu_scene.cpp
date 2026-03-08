68e0916220928266fb8019a20e5b98c2edd38220
TTboss90 <huisjestimon@gmail.com>
2026-03-08 20:36:56 +0100

    added "sub scenes"(a OPTIONS menu in main menu)

--- src/menu_scene.cpp
+++ src/menu_scene.cpp
@@ -6,7 +6,7 @@
 main_menu::main_menu()
 {
 	int i = 0;
-	for (button& the_button : m_buttons) {
+	for (button& the_button : m_main_buttons) {
 		i++;
 		the_button.rect.width = 200;
 		the_button.rect.height = 100;
@@ -21,14 +21,14 @@ main_menu::main_menu()
 			the_button.label = "Play";
 			the_button.on_click = [this, i]() {
 				std::cout << "Play" << std::endl;
-				m_buttons[i - 1].label = "Playing...";
+				next_scene = std::make_unique<game_scene>();
 				};
 			break;
 		case 2:
 			the_button.on_click = [this, i]() {
 				std::cout << "Button 2 Clicked!" << std::endl;
 				m_click_count++;
-				m_buttons[i - 1].label = "clickt " + std::to_string(m_click_count) + " times";
+				m_main_buttons[i - 1].label = "clickt " + std::to_string(m_click_count) + " times";
 				};
 			break;
 		case 3:
@@ -37,9 +37,10 @@ main_menu::main_menu()
 				};
 			break;
 		case 4:
+			the_button.label = "Options";
 			the_button.on_click = [this]() {
 				std::cout << "Button 4 Clicked!" << std::endl;
-				next_scene = std::make_unique<game_scene>();
+				m_menu_state = menu_state::OPTIONS;
 				};
 			break;
 		case 5:
@@ -57,13 +58,22 @@ main_menu::main_menu()
 
 void main_menu::update()
 {
-	if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-		Vector2 mouse_pos = GetMousePosition();
-		for (button& the_button : m_buttons) {
-			if (CheckCollisionPointRec(mouse_pos, the_button.rect)) {
-				the_button.on_click();
+	switch (m_menu_state)
+	{
+		case menu_state::MAIN:
+			if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+				Vector2 mouse_pos = GetMousePosition();
+				for (button& the_button : m_main_buttons) {
+					if (CheckCollisionPointRec(mouse_pos, the_button.rect)) {
+						the_button.on_click();
+					}
+				}
 			}
-		}
+			break;
+		case menu_state::OPTIONS:
+			if (IsKeyPressed(KEY_K))
+				m_menu_state = menu_state::MAIN;
+			break;
 	}
 }
 
@@ -71,17 +81,25 @@ void main_menu::draw_scene()
 {
 	DrawFPS(10, 10);
 
-	for (button& the_button : m_buttons)
+	switch (m_menu_state) 
 	{
-		DrawRectangleRec(the_button.rect, BLUE);
-		int fontSize = 30;
-		int textWidth = MeasureText(the_button.label.c_str(), fontSize);
+		case menu_state::MAIN:
+			for (button& the_button : m_main_buttons)
+			{
+				DrawRectangleRec(the_button.rect, BLUE);
+				int fontSize = 30;
+				int textWidth = MeasureText(the_button.label.c_str(), fontSize);
 
-		float textX = the_button.rect.x + (the_button.rect.width - textWidth) / 2.0f;
-		float textY = the_button.rect.y + (the_button.rect.height - fontSize) / 2.0f;
+				float textX = the_button.rect.x + (the_button.rect.width - textWidth) / 2.0f;
+				float textY = the_button.rect.y + (the_button.rect.height - fontSize) / 2.0f;
 
-		DrawText(the_button.label.c_str(), textX, textY, fontSize, BLACK);
+				DrawText(the_button.label.c_str(), textX, textY, fontSize, BLACK);
 
+			}
+			break;
+		case menu_state::OPTIONS:
+			DrawText("Options Menu", 10, 10, 40, RED);
+			break;
 	}
 }
 
--- src/menu_scene.hpp
+++ src/menu_scene.hpp
@@ -3,6 +3,12 @@
 #include "2d_tower_def.hpp"
 #include <array>
 
+enum class menu_state
+{
+	MAIN,
+	OPTIONS,
+};
+
 class main_menu : public scene
 {
 public:
@@ -13,7 +19,9 @@ public:
 	void update() override;
 
 private:
-	std::array<button, 5> m_buttons;
+	std::array<button, 5> m_main_buttons;
+
+	menu_state m_menu_state = menu_state::MAIN;
 
 	int m_click_count = 0;
 };
